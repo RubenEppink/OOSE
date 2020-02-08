7e92fac9381274300814e055ea9e708b8f99b698
Ruben <ruben_eppink@live.nl>
2020-02-08 10:47:47 +0100

    Stringkata add method refactored

--- DEA/Week 1/stringkatatest/src/main/java/ica/han/oose/StringCalculator.java
+++ DEA/Week 1/stringkatatest/src/main/java/ica/han/oose/StringCalculator.java
@@ -7,22 +7,20 @@ import java.util.stream.Collectors;
 public class StringCalculator {
 
     public int add(String numbers) {
-
-        List<String> numberList;
-
-        if (numbers.isBlank()) {
-            return 0;
-        }
+        List<Integer> numberList;
 
         try {
-            numberList = Arrays.stream(numbers.split("\\D+")).filter(i -> !i.isEmpty()).collect(Collectors.toList());
+            numberList = Arrays.stream(numbers.split("\\D+"))
+                    .filter(i -> !i.isEmpty())
+                    .mapToInt(Integer::parseInt)
+                    .filter(i -> i <= 1000)
+                    .boxed()
+                    .collect(Collectors.toList());
         } catch (NumberFormatException e) {
             return 0;
         }
-
-        List<Integer> integerLijst = numberList.stream().mapToInt(Integer::parseInt).filter(i -> i <= 1000).boxed().collect(Collectors.toList());
-
-        return integerLijst.stream()
+        
+        return numberList.stream()
                 .reduce(0, (a, b) -> a + b);
     }
 }
