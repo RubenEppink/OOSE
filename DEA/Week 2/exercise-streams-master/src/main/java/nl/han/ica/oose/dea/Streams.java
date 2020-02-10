8f5e10c710d959abdc44ab80823e22f59db72e99
Ruben <ruben_eppink@live.nl>
2020-02-10 17:52:11 +0100

    refactor stream oefening

--- DEA/Week 2/exercise-streams-master/src/main/java/nl/han/ica/oose/dea/Streams.java
+++ DEA/Week 2/exercise-streams-master/src/main/java/nl/han/ica/oose/dea/Streams.java
@@ -9,26 +9,41 @@ import java.util.stream.Collectors;
 class Streams {
 
     List<String> filterStringsLongerThanThreeCharacters(List<String> input) {
-        return input.stream().filter(i -> i.length() < 3).collect(Collectors.toList());
+        return input.stream()
+                .filter(i -> i.length() < 3)
+                .collect(Collectors.toList());
     }
 
     List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
-        return input.stream().filter(i -> i.matches("\\d+")).collect(Collectors.toList());
+        return input.stream()
+                .filter(i -> i.matches("\\d+"))
+                .collect(Collectors.toList());
     }
 
     String findShortestString(List<String> input) {
-        return input.stream().filter(word -> word.length() == input.stream().map(String::length).sorted().findFirst().get()).findFirst().get();
+        return input.stream()
+                .filter(word -> word.length() ==
+                        input.stream()
+                                .map(String::length)
+                                .sorted().findFirst()
+                                .get())
+                .findFirst()
+                .get();
     }
 
 
     String createAFullSentenceFromTheList(List<String> input) {
-        StringBuffer sb = new StringBuffer("");
+        /*StringBuffer sb = new StringBuffer("");
         input.forEach(i -> sb.append(i).append(" "));
 
         return sb.toString().trim();
+        */
+
+        return input.stream().collect(Collectors.joining(" "));
+
     }
 
     int calculateTotalCostOfAllProducts(List<Product> products) {
-        return products.stream().map(Product::getPrice).reduce(0, (a, b) -> a + b);
+        return products.stream().map(Product::getPrice).reduce(0, Integer::sum);
     }
 }
